8a20f00e54d457028ba540e140e313c92cbda4ff
Aditya Bhardwaj <Adityasir10@gmail.com>
2025-05-22 00:51:17 +0530

    testing-1

--- src/Pages/StartPage.jsx
+++ src/Pages/StartPage.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 
 const StartPage = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center px-4 py-8 bg-[#0A0A0A]">
-      <h1 className="text-center text-4xl sm:text-6xl md:text-7xl lg:text-8xl text-[#E6E6FA] font-neuton text-glow tracking-[0.1em] max-w-full">
-        Tic Tac Toe{" "}
-        <span className="font-aldrich text-[#FF004D] flicker-glow">2.o</span>
+    <div className="px-4 max-w-screen-xl mx-auto">
+      <h1 className="text-center text-4xl sm:text-6xl md:text-7xl lg:text-8xl text-[#E6E6FA] font-neuton mt-16 text-glow tracking-[0.1em]">
+        Tic Tac Toe <span className="font-aldrich text-[#FF004D] flicker-glow">2.o</span>
       </h1>
-      <h3 className="text-center text-[#00FFFF] text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-neuton tracking-widest mt-4 max-w-full">
+      <h3 className="text-center text-[#FFD700] text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-neuton tracking-widest mt-4">
         Hero Wars!
       </h3>
     </div>
